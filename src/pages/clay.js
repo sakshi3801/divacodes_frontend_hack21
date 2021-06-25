3c15be08dc7cf418eb461d47988ab994063276bc
dwdaditi <66900102+dwdaditi@users.noreply.github.com>
2021-06-25 18:58:38 +0530

    bootstrap updated

--- src/pages/clay.js
+++ src/pages/clay.js
@@ -1,137 +1,175 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
-import { CardGroup } from 'react-bootstrap';
+
 
 function clay(){
     return(
        
         <div>
-            <CardGroup   >
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This card has supporting text below as a natural lead-in to additional
-                            content.{' '}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to
-                            show that equal height action.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
-            <CardGroup   >
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This card has supporting text below as a natural lead-in to additional
-                            content.{' '}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to
-                            show that equal height action.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
-            <CardGroup   >
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This card has supporting text below as a natural lead-in to additional
-                            content.{' '}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px160" />
-                    <Card.Body>
-                        <Card.Title>Card title</Card.Title>
-                        <Card.Text>
-                            This is a wider card with supporting text below as a natural lead-in to
-                            additional content. This card has even longer content than the first to
-                            show that equal height action.
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Footer>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </Card.Footer>
-                </Card>
-            </CardGroup>
+            <CardDeck id='cardDeck'>
+                <Row id='cardRow' >
+                    <Col md={4} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQklc4KnL4xRNxVnLPZfjApnrwmV7I9kMtPJy9ePHLwJrt9i77UmJShGp_zPfaxOBJ18cxWOMMi_-66m0x4v17fX8idG_MMVXyG4BVVEUI&usqp=CAE" className='cardImg' />
+
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+                                    
+                                    
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                        </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTK7or4U6Xq0hXgA-zTzusw4E5pOy8cT_-bLkfSuCAsouFzZg0RIGP8rJpNLz59khE1F_VDANEJKrJ0rZ2sxTM02QJfoLwTPVpIXrrCe20&usqp=CAE" className='cardImg' />
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+                                   
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={3} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src={metal} className='cardImg' />
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+                                    
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    </Row>
+                <Row id='cardRow' >
+                    <Col md={4} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src={clay} className='cardImg' />
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+                                    </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src={clay} className='cardImg' />
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+                                   
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src={clay} className='cardImg' />
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+                                    
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row id='cardRow' >
+                    <Col md={4} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQklc4KnL4xRNxVnLPZfjApnrwmV7I9kMtPJy9ePHLwJrt9i77UmJShGp_zPfaxOBJ18cxWOMMi_-66m0x4v17fX8idG_MMVXyG4BVVEUI&usqp=CAE" className='cardImg' />
+
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+
+
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={4} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src="https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTK7or4U6Xq0hXgA-zTzusw4E5pOy8cT_-bLkfSuCAsouFzZg0RIGP8rJpNLz59khE1F_VDANEJKrJ0rZ2sxTM02QJfoLwTPVpIXrrCe20&usqp=CAE" className='cardImg' />
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={3} className='cardCol'>
+                        <Card className="focus mt-2 mb-2" id='section'>
+                            <Card.Img variant="top" src={metal} className='cardImg' />
+                            <Card.Body className='cardBody'>
+                                <Card.Title className="title text-center  card-title">Clay Items</Card.Title>
+                                <Card.Text>
+
+                                </Card.Text>
+                                <Button id='wallButton' variant="danger" size="lg" block><div><a href="https://vincraft.in/">Go to Website</a></div></Button>
+                                <Button id='wallButton' variant="danger" size="lg" block>
+                                    <div>
+                                        <a href="https://vincraft.in/home-decor/indian-handicrafts/handicraft-indian-cow-incense/">Buy Now</a>
+                                    </div>
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    </Row>
+            </CardDeck>
         </div>
 
     )
