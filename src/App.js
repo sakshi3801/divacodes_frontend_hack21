32b64fd5f20106e7c4aff8e65c831ef60301a7d8
Sakshi Ramsinghani <64496690+sakshi3801@users.noreply.github.com>
2021-06-25 17:44:54 +0530

    best sellers category added on the home screen

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp.jsx'
 import Carousal from './Components/Carousal';
 import ShopByCategory from './Components/ShopByCategory.jsx';
+import BestSellers from './Components/BestSellers';
 function App() {
   return (
     <div className="App">
     <NavbarComp />
     <Carousal />
     <ShopByCategory />
+    <BestSellers />
     </div>
   );
 }
