4d126cceb0ebb7bf97452d8ad4663f5edc59229b
Sakshi Ramsinghani <64496690+sakshi3801@users.noreply.github.com>
2021-06-25 14:46:47 +0530

    carousal addition

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp.jsx'
+import Carousal from './Components/Carousal';
 function App() {
   return (
     <div className="App">
     <NavbarComp />
+    <Carousal />
     </div>
   );
 }
