a606fcb6bf9e8f2ee159506034e21bec9d36d5ba
Sakshi Ramsinghani <64496690+sakshi3801@users.noreply.github.com>
2021-06-25 15:34:22 +0530

    shop by category component added on the home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp.jsx'
 import Carousal from './Components/Carousal';
+import ShopByCategory from './Components/ShopByCategory.jsx';
 function App() {
   return (
     <div className="App">
     <NavbarComp />
     <Carousal />
+    <ShopByCategory />
     </div>
   );
 }
