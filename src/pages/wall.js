9f5c6d023e678d5e479df3a6f442c4b54986f1fd
Sakshi Ramsinghani <64496690+sakshi3801@users.noreply.github.com>
2021-06-26 19:04:42 +0530

    initial code of wall product added

--- src/pages/wall.js
+++ src/pages/wall.js
@@ -7,37 +7,39 @@ import Button from 'react-bootstrap/Button'
 import NavbarComp from '../Components/NavbarComp'
 import Footer from '../Components/Footer'
 import { ShoppingCart} from '@material-ui/icons';
-import wall1 from '../images/product/wall1.jpeg'
+import wall1 from '../images/product/wall9.jpeg'
 import wall2 from '../images/product/wall2.jpg'
 import wall3 from '../images/product/wall3.jpg'
-import metal4 from '../images/product/metal19.jpg'
-import metal5 from '../images/product/metal4.jpg'
-import metal6 from '../images/product/metal18.jpg'
-import metal9 from '../images/product/metal9.jpg'
-import metal12 from '../images/product/metal12.jpg'
+import wall4 from '../images/product/wall4.jpg'
+import wall5 from '../images/product/wall5.jpg'
+import wall6 from '../images/product/wall6.jpg'
+import wall7 from '../images/product/wall7.jpg'
+import wall8 from '../images/product/wall8.jpg'
 import metal14 from '../images/product/metal14.jpg'
 import LanguageIcon from '@material-ui/icons/Language';
 import './product.css'
 
-function Lamps() {
+function Wall() {
     return (
         <>
         <NavbarComp />
         <div id='shop'>
-        <h2 id='category'>Metal-Lustre and Strength together</h2>
+        <h2 id='category'>Wall- Give your walls an artistic touch</h2>
 <div id='card'>
        <CardDeck id='cardDeck'>
        <Row id='cardRow' >
+
        <Col md={3} className='cardCol'>
-           <Card className="focus mt-2 mb-2" id='sec'>
-           <Card.Img variant="top" src={wall1} className='cardImg' />
+       <Card className="focus mt-2 mb-2" id='sec'>
+           <Card.Img variant="top" src={wall8} className='cardImg' />
            <Card.Body className='cardBody'>
-               <Card.Title className="title text-center  card-title">Pink Acacia Wood Net Floral Series Wall Plate</Card.Title>
+               <Card.Title className="title text-center  card-title">Hippy Mandala Wall Hanging Decorative Handmade Tapestry
+</Card.Title>
                <Card.Text id='cardText'>
-     <span id='price1'>Rs 2000.00</span> <h5 id = 'price2'>Rs. 1850.00</h5>
+     <span id='price1'>Rs 375.00</span> <h5 id = 'price2'>Rs. 250.00</h5>
    </Card.Text>
-   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.jaypore.com/'>Visit Website </a><LanguageIcon /></Button>
-   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.jaypore.com/pink-acacia-wood-net-floral-series-wall-plate-p724826'>Buy Now</a></Button>
+   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.indiamart.com/'>Visit Website </a><LanguageIcon /></Button>
+   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.indiamart.com/proddetail/hippy-mandala-wall-hanging-decorative-handmade-tapestry-17715880155.html'>Buy Now</a></Button>
            </Card.Body>
            </Card>
        </Col>
@@ -45,7 +47,8 @@ function Lamps() {
        <Card className="focus mt-2 mb-2" id='sec'>
            <Card.Img variant="top" src={wall2} className='cardImg' />
            <Card.Body className='cardBody'>
-               <Card.Title className="title text-center  card-title">The Jaipur Royal Elephant Table Decoration Showpiece</Card.Title>
+               <Card.Title className="title text-center  card-title">Antique Malhar Wall Screen (L - 7.5in ,W - 3in ,H - 21.5in)
+</Card.Title>
                <Card.Text id='cardText'>
      <span id='price1'>Rs 1212.00</span> <h5 id = 'price2'>Rs. 1000.00</h5>
    </Card.Text>
@@ -72,14 +75,14 @@ function Lamps() {
 
        <Col md={3} className='cardCol'>
        <Card className="focus mt-2 mb-2" id='sec'>
-           <Card.Img variant="top" src={metal4} className='cardImg' />
+           <Card.Img variant="top" src={wall4} className='cardImg' />
            <Card.Body className='cardBody'>
-               <Card.Title className="title text-center  card-title">Brass Lord Ganesha on Rocking Chair Antique Showpiece</Card.Title>
+               <Card.Title className="title text-center  card-title">LOVE BIRDS SATIN MATT TEXTURE UV ART PAINTING</Card.Title>
                <Card.Text id='cardText'>
-     <span id='price1'>Rs 2300.00</span> <h5 id = 'price2'>Rs. 2000.00</h5>
+     <span id='price1'>Rs 899.00</span> <h5 id = 'price2'>Rs. 530.00</h5>
    </Card.Text>
-   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.ajio.com/'>Visit Website </a><LanguageIcon /></Button>
-   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.ajio.com/ecraftindia-brass-lord-ganesha-on-rocking-chair-antique-showpiece/p/461557104_brown'>Buy Now</a></Button>
+   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.mirraw.com/'>Visit Website </a><LanguageIcon /></Button>
+   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.mirraw.com/designers/ecraftindia/designs/love-birds-satin-matt-texture-uv-art-painting'>Buy Now</a></Button>
            </Card.Body>
            </Card>
        </Col>
@@ -88,55 +91,56 @@ function Lamps() {
        <Row id='cardRow' >
        <Col md={3} className='cardCol'>
            <Card className="focus mt-2 mb-2" id='sec'>
-           <Card.Img variant="top" src={metal5} className='cardImg' />
+           <Card.Img variant="top" src={wall5} className='cardImg' />
            <Card.Body className='cardBody'>
-               <Card.Title className="title text-center  card-title">Textured Tall Single Pc. Vase - Stoneware - Gold</Card.Title>
+               <Card.Title className="title text-center  card-title">Sunrise View Decorative Wooden Handcrafted Wall Clock
+</Card.Title>
                <Card.Text id='cardText'>
-     <span id='price1'>Rs 2000.00</span> <h5 id = 'price2'>Rs. 1850.00</h5>
+     <span id='price1'>Rs 1899.00</span> <h5 id = 'price2'>Rs. 1199.00</h5>
    </Card.Text>
-   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.homecentre.in/'>Visit Website </a><LanguageIcon /></Button>
-   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.homecentre.in/in/en/Decor/Home-Accessories/Vases/HOMECENTRE-Stellar-Fantasy-N-Celestial-Textured-Tall-Single-Pc-Vase--Stoneware--Gold/p/1000008417741'>Buy Now</a></Button>
+   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.ecraftindia.com/'>Visit Website </a><LanguageIcon /></Button>
+   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.ecraftindia.com/products/ecraftindia-sunrise-view-decorative-wooden-handcrafted-wall-clock'>Buy Now</a></Button>
            </Card.Body>
            </Card>
        </Col>
        <Col md={3} className='cardCol'>
        <Card className="focus mt-2 mb-2" id='sec'>
-           <Card.Img variant="top" src={metal6} className='cardImg' />
+           <Card.Img variant="top" src={wall6} className='cardImg' />
            <Card.Body className='cardBody'>
-               <Card.Title className="title text-center  card-title">Peacock Green Hand Painted Brass Utsav Masaldan
+               <Card.Title className="title text-center  card-title">Set of 3 Floral and Botanical Satin Matt Textured UV Art Painting
 </Card.Title>
                <Card.Text id='cardText'>
      <span id='price1'>Rs 3430.00</span> <h5 id = 'price2'>Rs. 2990.00</h5>
    </Card.Text>
-   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.jaypore.com/'>Visit Website </a><LanguageIcon /></Button>
-   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.jaypore.com/peacock-green-and-gold-hand-painted-brass-utsav-masaldan-h-175-in-dia-65in-p671829'>Buy Now</a></Button>
+   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.ajio.com/'>Visit Website </a><LanguageIcon /></Button>
+   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.ajio.com/ecraftindia-set-of-3-floral-and-botanical-satin-matt-textured-uv-art-painting/p/461804512_blue'>Buy Now</a></Button>
            </Card.Body>
            </Card>
        </Col>
        <Col md={3} className='cardCol'>
        <Card className="focus mt-2 mb-2" id='sec'>
-           <Card.Img variant="top" src={metal14} className='cardImg' />
+           <Card.Img variant="top" src={wall7} className='cardImg' />
            <Card.Body className='cardBody'>
-               <Card.Title className="title text-center  card-title">Brass Urli (L- 8in, W- 9in, H- 9.5in)</Card.Title>
-               <Card.Text id='cardText'>
-     <span id='price1'>Rs 2300.00</span> <h5 id = 'price2'>Rs. 2000.00</h5>
-   </Card.Text>
-   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.jaypore.com/'>Visit Website </a><LanguageIcon /></Button>
-   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.jaypore.com/brass-urli-l-8in-w-9in-h-95in-p585573'>Buy Now</a></Button>
-           </Card.Body>
-           </Card>
-       </Col>
-       <Col md={3} className='cardCol'>
-       <Card className="focus mt-2 mb-2" id='sec'>
-           <Card.Img variant="top" src={metal12} className='cardImg' />
-           <Card.Body className='cardBody'>
-               <Card.Title className="title text-center  card-title">AUTHENTIC DOKRA ART FROM ODISHA - DANCING WOMEN PAIR
+               <Card.Title className="title text-center  card-title">Colorful and Vibrant Feather Dream Catcher - Handmade
 </Card.Title>
                <Card.Text id='cardText'>
-     <span id='price1'>Rs 2300.00</span> <h5 id = 'price2'>Rs. 2000.00</h5>
+     <span id='price1'>Rs 655.00</span> <h5 id = 'price2'>Rs. 450.00</h5>
    </Card.Text>
-   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.artisansoul.in/'>Visit Website </a><LanguageIcon /></Button>
-   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.artisansoul.in/products/authentic-dokra-art-from-odisha-dancing-women'>Buy Now</a></Button>
+   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://artbodymind.com/'>Visit Website </a><LanguageIcon /></Button>
+   <Button id= 'button2' variant="danger"><a id='buy' href='https://artbodymind.com/products/antique-handmade-dream-catcher-peacock-feather-wall-hanging-decoration-wind-chimes-ornament-25-long'>Buy Now</a></Button>
+           </Card.Body>
+           </Card>
+       </Col>
+       <Col md={3} className='cardCol'>
+           <Card className="focus mt-2 mb-2" id='sec'>
+           <Card.Img variant="top" src={wall1} className='cardImg' />
+           <Card.Body className='cardBody'>
+               <Card.Title className="title text-center  card-title">Pink Acacia Wood Net Floral Series Wall Plate - Handmade</Card.Title>
+               <Card.Text id='cardText'>
+     <span id='price1'>Rs 2000.00</span> <h5 id = 'price2'>Rs. 1850.00</h5>
+   </Card.Text>
+   <Button id= 'button1' variant="outline-secondary"><a id='buy1' href='https://www.jaypore.com/'>Visit Website </a><LanguageIcon /></Button>
+   <Button id= 'button2' variant="danger"><a id='buy' href='https://www.jaypore.com/pink-acacia-wood-net-floral-series-wall-plate-p724826'>Buy Now</a></Button>
            </Card.Body>
            </Card>
        </Col>
@@ -149,4 +153,4 @@ function Lamps() {
     )
 }
 
-export default Lamps
+export default Wall
